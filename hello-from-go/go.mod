7f3321839909f68af10054e55586b68372669637
Benjamin Eckel <bhelx@simst.im>
2023-12-11 12:39:15 -0600

    add node, updates

--- hello-from-go/go.mod
+++ hello-from-go/go.mod
@@ -3,6 +3,6 @@ module dylibso.com/hello-from-go
 go 1.21.1
 
 require (
-	github.com/extism/go-pdk v0.0.0-20230921221315-91d257ab7bda // indirect
+	github.com/extism/go-pdk v1.0.0-rc2 // indirect
 	github.com/valyala/fastjson v1.6.3 // indirect
 )
--- hello-in-python/app.py
+++ hello-in-python/app.py
@@ -9,11 +9,27 @@ def manifest(filename):
     return {"wasm": [{"path": os.path.join(WASM_DIR, filename)}]}
 
 def load_plugins():
-    return [
-        extism.Plugin(manifest(filename), wasi=True)
-        for filename in os.listdir(WASM_DIR)
-        if filename.endswith(".wasm")
-    ]
+    # return [
+    #     extism.Plugin(manifest(filename), wasi=True)
+    #     for filename in os.listdir(WASM_DIR)
+    #     if filename.endswith(".wasm")
+    # ]
+    files = [
+            filename
+            for filename in os.listdir(WASM_DIR)
+            if filename.endswith(".wasm")
+            ]
+    plugins = []
+    for filename in files:
+        print(filename)
+        if filename.endswith("hello-from-c.wasm"):
+            continue
+        if filename.endswith("hello-from-haskell.wasm"):
+            continue
+        plugin = extism.Plugin(manifest(filename), wasi=True)
+        plugins.append(plugin)
+
+    return plugins
 
 def run_plugins(inpt):
     return reduce(lambda i, p: p.call('hello', i), plugins, inpt)
@@ -25,7 +41,7 @@ plugins = load_plugins()
 print(f"Loaded {len(plugins)} plugins in {int((time.time() - start) * 1000)} ms")
 
 while True:
-    inpt = str.encode("Hello from Python!")
+    inpt = str.encode("Hello in Python!")
     input("Run plug-ins [Enter]> ")
     start = time.time()
     inpt = run_plugins(inpt)
